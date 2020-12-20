5a323abefbf45810c80c6a9007e1ea907166db3c
Sebastian G. Q <sgq995@gmail.com>
2020-12-19 19:45:37 -0500

    feat: Added testing for universe generator and tick

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,17 @@ impl Universe {
         }
         count
     }
+
+    pub fn get_cells(&self) -> &[u32] {
+        self.cells.as_slice()
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
 }
 
 #[wasm_bindgen]
@@ -104,8 +115,24 @@ impl Universe {
         self.width
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        // self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+        // self.cells.grow((width * self.height) as usize);
+        let size = (width * self.height) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
+    }
+
     pub fn height(&self) -> u32 {
         self.height
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        // self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+        // self.cells.grow((self.width * height) as usize);
+        let size = (self.width * height) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
     }
 
     pub fn cells(&self) -> *const u32 {
